c2a944aa171d09a11f51e3886a16ee432c8b55ef
L-Chris <563303226@qq.com>
2018-08-08 23:26:34 +0800

    update: 删除多余文件

--- src/plugins/index.js
+++
@@ -1,9 +0,0 @@
-const files = require.context('.', false, /\.js$/)
-const modules = {}
-
-files.keys().forEach(key => {
-  if (key.includes('/index')) return
-  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
-
-export default vue => Object.entries(modules).map(([, install]) => install(vue))
